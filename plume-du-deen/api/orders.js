e16ce8a7811fba9b9aa9c246e4882dc8bf5fabe7
bilel-8051 <bilel@mail.com>
2026-02-20 10:01:19 +0100

    Add commit header to orders API

--- plume-du-deen/api/orders.js
+++ plume-du-deen/api/orders.js
@@ -123,6 +123,10 @@ export default async function handler(req, res) {
   // Appliquer les headers de sécurité
   setSecurityHeaders(req, res);
 
+  if (process.env.VERCEL_GIT_COMMIT_SHA) {
+    res.setHeader('X-Plume-Commit', process.env.VERCEL_GIT_COMMIT_SHA);
+  }
+
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
